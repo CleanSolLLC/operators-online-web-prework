0eb7c6b53c43fdbe02f34e73ee0a7993a7d48880
Mark May <mamark90@gmail.com>
2019-03-01 19:40:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,6 +8,6 @@ end
 
 
 def not_safe?(speed)
-	if speed < 40 ? return true : return false
-  if speed > 60 ? return true : return false
+	if speed < 40 ? true : false
+  if speed > 60 ? true : false
 end
